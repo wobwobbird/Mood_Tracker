be88f9d7f433928d9fe0714f27bfc90914674264
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2026-01-29 11:33:45 +0000

    created colourSheme to manage the colours

---
+++ colourScheme.js
@@ -0,0 +1,21 @@
+export const logoColourSchemes = [
+    ["blue", "blueBright", "cyan", "cyanBright", "blue", "cyan", "blueBright", "cyanBright", "blue", "cyan", "blueBright"],
+    ["green", "greenBright", "cyanBright", "green", "greenBright", "cyanBright", "green", "greenBright", "cyanBright", "green", "greenBright"],
+    ["red", "red", "white", "white", "blue", "blue", "red", "red", "white", "white", "blue"],
+    ["redBright", "red", "magenta", "magentaBright", "yellow", "yellowBright", "cyan", "cyanBright", "blue", "blueBright", "whiteBright"],
+    ["magentaBright", "magentaBright", "magenta", "magenta", "magentaBright", "magentaBright", "magenta", "magenta", "magentaBright", "magentaBright", "magenta"], // neon pink and pink
+    ["white", "white", "white", "white", "white", "red", "red", "red", "red", "red", "red"],
+    ["white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white"],
+    ["black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black"]
+];
+
+export const borderColourSchemes = [
+    ["blue", "blueBright", "cyan", "cyanBright"],
+    ["green", "greenBright", "cyanBright", "green"],
+    ["red", "white", "blue", "white"],
+    ["redBright", "magentaBright", "yellowBright", "cyanBright"],
+    ["magentaBright", "magenta", "magentaBright", "magenta"],
+    ["white", "white", "red", "red"],
+    ["white", "white", "white", "white"],
+    ["black", "black", "black", "black"]
+];
